0c340b31e5bce16b823b95d208666fc96e39482b
Suzanne Schuber <suzanneschuber@gmail.com>
2023-09-08 16:26:37 -0700

    add NewFabricForm field variables, WIP on how to add an image through the form.

--- src/Components/NewFabicForm.js
+++ src/Components/NewFabicForm.js
@@ -1,14 +1,42 @@
 import React from "react";
 
-function NewFabricForm (props){
+function NewFabricForm() {
   return (
     <React.Fragment>
       <form onSubmit={handleNewFabricFormSubmission}>
 
+        <input
+          type="text"
+          name="description"
+          placeholder="short description" />
 
-        
+        <input
+          type="text"
+          name="price"
+          placeholder="price per yard" />
+
+        <textarea
+          name="details"
+          placeholder="Add details about the fabric." />
+
+        <input
+          type="text"
+          name="yards"
+          placeholder="How many yards to add?" />
+
+          <input
+          type="file"
+          name="image"                  
+          />
+          <button type="submit">Add Fabric</button>
       </form>
     </React.Fragment>
   );
+
+  function handleNewFabricFormSubmission(event) {
+    event.preventDefault();
+    console.log(event.target.description.value)
+  }
+
 }
 export default NewFabricForm;
