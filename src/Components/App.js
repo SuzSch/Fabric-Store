63d2500c8f21ed11f019a43f40957fccd71889ec
Suzanne Schuber <suzanneschuber@gmail.com>
2023-09-08 10:27:30 -0700

    Add prop types to fabric component, WIP list of hard-coded fabrics in fabricList component.

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Header";
+import FabricList from "./FabricList";
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <FabricList />
     </React.Fragment>
   );
 }
--- src/Components/FabricList.js
+++ src/Components/FabricList.js
@@ -1,10 +1,54 @@
 import React from "react";
 import Fabric from "./Fabric";
 
+const fabricsInStock = [
+  {
+    description:
+    price:
+    details:
+    yardsInStock:
+  },
+  {
+    description:
+      price:
+    details:
+      yardsInStock:
+  },
+  {
+    description:
+      price:
+    details:
+      yardsInStock:
+  },
+  {
+    description:
+      price:
+    details:
+      yardsInStock:
+  },
+  {
+    description:
+      price:
+    details:
+      yardsInStock:
+  },
+  {
+    description:
+      price:
+    details:
+      yardsInStock:
+  }
+
+];
+
 function FabricList() {
   return (
-    <Fabric />
+    <Fabric
+      description="8oz Organic Sanded Twill"
+      price="$20.25 /yd"
+      details="This organic cotton has a fine twill with a soft sanded finish on one side."
+      yardsInStock= "30"
+    />
   );
 }
-
 export default FabricList;
